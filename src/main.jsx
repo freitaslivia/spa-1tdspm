747aab534dcec0b6a4a0560e029d1c3847bc765c
freitaslivia <xliviafreitasx@gmail.com>
2023-09-01 09:05:39 -0300

    Iniciando a construção de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from "./routes/Home"
+import Produtos from "./routes/Produtos"
+import Erro404 from "./routes/Erro404"
+import EditarProdutos from "./routes/EditarProdutos"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
