738ba96dc9ef8cd6617dd980337eda3383e447b5
freitaslivia <xliviafreitasx@gmail.com>
2023-09-25 11:16:01 -0300

    estilização da imagem

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useState } from "react";
 import { ListaProdutos } from "../components/ListaProdutos";
-import classes from "./ExcluirProdutos.modules.css"
+import classes from "./ExcluirProdutos.module.css"
 
 export default function ExcluirProduto() {
   document.title = "Excluir Produto";
@@ -48,5 +48,5 @@ export default function ExcluirProduto() {
             </div>
         </div>
       </div>
-      );
+      )
 }
