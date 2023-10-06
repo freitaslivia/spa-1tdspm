a6cb9c2ea9f0e3a8fdc670268dd95653bda34eb8
xliviafreitasx@gmail.com <xliviafreitasx@gmail.com>
2023-10-06 09:54:15 -0300

    style css canbecalho com sass

---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,28 @@
+import { Link, useLocation } from "react-router-dom";
+import {AiFillHome as H} from "react-icons/ai";
+import "./Cabecalho.scss";
+
+export default function Cabecalho() {
+
+  const rotaAtual = useLocation();
+
+  console.log("rota atual: " + rotaAtual.pathname)
+
+  return (
+    <>
+      <header className="cabecalho">
+        <ul>
+          <li>
+            <Link to="/" className={rotaAtual.pathname == "/" ? "active" : ""}>HOME - <H/></Link>
+          </li>
+          <li>
+            <Link to="/produtos" className={rotaAtual.pathname == "/produtos" ? "active" : ""}>PRODUTOS</Link>
+          </li>
+          <li>
+            <Link to="/cadastrar/produto" className={rotaAtual.pathname == "/cadastrar/produto" ? "active" : ""}>CADASTRAR PRODUTO</Link>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+}
