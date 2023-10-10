67eaae57e8843b59ee60b8b362af36e848a7c4ac
freitaslivia <xliviafreitasx@gmail.com>
2023-10-10 19:41:58 -0300

    editar e excluir feito

---
+++ src/components/EditarP/EditarP.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+
+export default function EditarProduto(props) {
+  const [produto, setProduto] = useState({
+    id: "",
+    nome: "",
+    desc: "",
+    valor: "",
+  });
+
+  // UseEffect para carregar os dados do produto quando o componente é montado
+  useEffect(() => {
+    if (props.produto) {
+      setProduto(props.produto);
+    }
+  }, [props.produto]);
+
+  const handleChange = (e) => {
+    // Destructuring dos dados que chegam do form.
+    const { name, value } = e.target;
+    setProduto({ ...produto, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(`http://localhost:5000/produtos/${produto.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(produto),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        // Feche o modal de edição
+        props.setOpenEditar(false);
+
+        location.reload();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  if (props.openEditar) {
+    return (
+      <div>
+        <h1>Editar Produto</h1>
+
+        <button onClick={()=> props.setOpenEditar(false)}>X</button>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="nome">Nome do Produto:</label>
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              value={produto.nome}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="desc">Descrição do Produto:</label>
+            <input
+              type="text"
+              id="desc"
+              name="desc"
+              value={produto.desc}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="valor">Valor do Produto:</label>
+            <input
+              type="text"
+              id="valor"
+              name="valor"
+              value={produto.valor}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </div>
+    );
+  }
+}
